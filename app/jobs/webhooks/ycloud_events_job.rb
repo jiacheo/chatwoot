7ed18682f6368e402a40d098df8f344ae980af50
jiacheo <jiacheo@taovip.com>
2022-11-15 15:37:00 +0800

    add log for debug

--- app/jobs/webhooks/ycloud_events_job.rb
+++ app/jobs/webhooks/ycloud_events_job.rb
@@ -2,11 +2,13 @@ class Webhooks::YcloudEventsJob < ApplicationJob
   queue_as :default
 
   def perform(params = {})
-    Rails.logger.info("receive ycloud event:" + params.to_s)
+    
     if params[:type] == 'whatsapp.inbound_message.received'
       if params[:whatsappInboundMessage]
         phone_number = params[:whatsappInboundMessage][:to]
+        Rails.logger.info("-------receive ycloud event------- phone number is" + phone_number)
         channel = Channel::Whatsapp.find_by(phone_number: params[:phone_number])
+        Rails.logger.info("-------receive ycloud event------- channel is " + (channel?channel.inspect.to_s:"null"))
         return if channel.blank?
         Rails.logger.info("found channel:" + channel.inspect.to_s + ", now do inbox saving")
         #todo: verify the incoming message with channel webhook_token
