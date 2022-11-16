47ee7c7cd3491ea2493c033310b3b2b9ad3868b6
jiacheo <jiacheo.wu@gmail.com>
2022-11-16 14:22:47 +0800

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,8 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
-  config.active_job.queue_adapter = :inline
+  #config.active_job.queue_adapter = :inline
+  config.active_job.queue_adapter = :async
   # config.active_job.queue_name_prefix = "Chatwoot_#{Rails.env}"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
